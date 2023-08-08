d9658298996f4df7b3a3f6b25d2182b250803be3
Joowon LEE <joowon0410@naver.com>
2023-08-08 17:01:43 +0900

    백준 13975 파일 합치기 주석 추가

--- algorithm/baekjoon/FileMerge13975Baekjoon.java
+++ algorithm/baekjoon/FileMerge13975Baekjoon.java
@@ -18,6 +18,8 @@ public class FileMerge13975Baekjoon {
 		StringBuilder sb = new StringBuilder();
 		for(int testCase = 0 ; testCase < test ; testCase++) {
 			int n = Integer.parseInt(br.readLine());
+			// 우선 제일 작은 숫자들이 제일 많이 더해지면 된다는 생각으로 접근을 하였다.
+			// 제일 작은 숫자들을 얻기 위해서 제일 빨리 작은 수를 찾을 수 있는 우선순위 큐를 이용하자.
 			PriorityQueue<Long> pQue= new PriorityQueue<>();
 
 			st = new StringTokenizer(br.readLine());
@@ -26,6 +28,8 @@ public class FileMerge13975Baekjoon {
 			}
 			
 			long sum = 0;
+			
+			// 우선순위 큐에 마지막 값이 나올 때까지 매 순간 제일 작은 숫자 둘을 더해준다.
 			while(pQue.size() != 1) {
 				long temp = pQue.poll();
 				temp += pQue.poll();
