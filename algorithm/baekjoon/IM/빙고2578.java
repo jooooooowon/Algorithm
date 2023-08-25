ab1162ca0e88aa53b9e452b51dbe51eac09be96c
jooooooowon <joowon0410@naver.com>
2023-08-25 15:46:20 +0900

    2023-08-25 IM 문제

--- algorithm/baekjoon/IM/빙고2578.java
+++ algorithm/baekjoon/IM/빙고2578.java
@@ -1,15 +1,18 @@
 package Algorithm.algorithm.baekjoon.IM;
 
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class 빙고2578 {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		int[][] arr = new int[5][5];
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		for(int bingo = 0; bingo < 25 ; bingo++) {
 			st = new StringTokenizer(br.readLine());
+			
 		}
 	}
 }
