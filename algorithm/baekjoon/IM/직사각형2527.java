e7ed4f5f076cb1d3f99db87168268caf1e185fcd
DESKTOP-EAPE4TG\USER <joowon0410@naver.com>
2023-09-01 20:03:07 +0900

    2023-09-1

--- algorithm/baekjoon/IM/직사각형2527.java
+++ algorithm/baekjoon/IM/직사각형2527.java
@@ -6,15 +6,47 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class 직사각형2527 {
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st ;
-		for(int test = 0 ; test < 4 ; test++) {
-			st = new StringTokenizer(br.readLine());
-			int x = Integer.parseInt(st.nextToken());
-			int y = Integer.parseInt(st.nextToken());
-			int p = Integer.parseInt(st.nextToken());
-			int q = Integer.parseInt(st.nextToken());
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < 4; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int x_1 = Integer.parseInt(st.nextToken());
+			int y_1 = Integer.parseInt(st.nextToken());
+			int p_1 = Integer.parseInt(st.nextToken());
+			int q_1 = Integer.parseInt(st.nextToken());
+
+			int x_2 = Integer.parseInt(st.nextToken());
+			int y_2 = Integer.parseInt(st.nextToken());
+			int p_2 = Integer.parseInt(st.nextToken());
+			int q_2 = Integer.parseInt(st.nextToken());
+			if (x_1 > p_2 || p_1 < x_2 || y_1 > q_2 || q_1 < y_2)
+				sb.append("d\n");
+			else if (x_1 == p_2) {
+				if (y_1 == q_2 || y_2 == q_1)
+					sb.append("c\n");
+				else
+					sb.append("b\n");
+			} else if (p_1 == x_2) {
+				if (q_1 == y_2 || q_2 == y_1)
+					sb.append("c\n");
+				else
+					sb.append("b\n");
+			} else if (y_1 == q_2) {
+				if (x_1 == p_2 || x_2 == p_1)
+					sb.append("c\n");
+				else
+					sb.append("b\n");
+			} else if (q_1 == y_2) {
+				if (p_1 == x_2 || p_2 == x_1)
+					sb.append("c\n");
+				else
+					sb.append("b\n");
+			} else
+				sb.append("a\n");
 		}
+		System.out.println(sb);
 	}
+
 }
