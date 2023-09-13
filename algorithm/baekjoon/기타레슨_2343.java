b4ea2605a03095f0256db859c7d3d7617fb6fc54
jooooooowon <joowon0410@naver.com>
2023-09-13 17:58:31 +0900

    이상한술집, 예산 문제 풀이 완료, 기타레슨 문제 풀이 중

---
+++ algorithm/baekjoon/기타레슨_2343.java
@@ -0,0 +1,27 @@
+package Algorithm.algorithm.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class 기타레슨_2343 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		int[] arr = new int[n];
+		st = new StringTokenizer(br.readLine());
+		int max = 0;
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+			max += arr[i];
+		}
+		Arrays.sort(arr);
+		for (int i = 0; i < n - m + 1; i++) {
+
+		}
+	}
+}
